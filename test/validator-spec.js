d85c7268f3f8e487b47196ce170480e55af292a2
ufuchs <ufuchs@gmx.com>
2014-04-07 22:53:05 +0200

    working on 'validator'

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -11,12 +11,13 @@ var validator = require('../lib/value-validator'),
 
 order.customerID = 1034056;
 order.orderID = 5670876;
-order.orderDate = new Date()
-    .toISOString()
-    .replace(/^(\d{4})\-(\d{2})\-(\d{2}).*$/, '$2/$3/$1');
+order.orderDate = '2014-01*01';
+// order.orderDate = new Date()
+//     .toISOString()
+//     .replace(/^(\d{4})\-(\d{2})\-(\d{2}).*$/, '$2/$3/$1');
 order.item = 'ATmega328P-PU';
-order.qty = 10;
-order.price = 2.02;
+order.qty = '0000';
+order.price = '2.0X';
 order.note = "special customer with 5% discount         \n       ";
 
 validator().validate(order, descriptors);
