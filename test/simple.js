7c34b9c467c5b244da2e53f6947ce361d1cea3f8
ufuchs <ufuchs@gmx.com>
2014-04-13 19:55:31 +0200

    adjustments for 'oilInvo'

--- test/simple.js
+++ test/simple.js
@@ -19,7 +19,7 @@ console.log(invo);
 console.log("\n2. Now fill in some data in your 'invoice' object");
 console.log("-------------------------------------------------\n");
 
-invo.customerID = 1034056;
+invo.customerID = 4056;
 invo.invoiceDate = new Date()
     .toISOString()
     .replace(/^(\d{4})\-(\d{2})\-(\d{2})\S*$/, '$2/$3/$1');
@@ -28,38 +28,24 @@ invo.text = "You will get 10% discount";
 
 console.log(invo);
 
-var            formatter = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-                minimumFractionDigits: 2,
-            });
+console.log("\n4.0 Validate the 'invoice' object against the model");
+console.log("---------------------------------------------------\n");
 
+serializer().validate(invo, descriptors);
 
-formatter.style = 'currency';
+console.log("  If you doesn't see any output, feel lucky!\n");
 
-console.log(new Intl.NumberFormat("de-DE",{maximumSignificantDigits: 3}).format(111111.00));
+console.log("\n5.0 Serialize the 'invoice' object for your legacy bookkeeping system");
+console.log("---------------------------------------------------------------------\n");
 
-
-
-
-/*
-
-console.log("\n4.0 Validate the 'order' object against model.");
-console.log("----------------------------------------------\n");
-
-serializer().validate(order, descriptors);
-
-console.log("\n  If you doesn't see any output, feel lucky!\n");
-
-console.log("\n5.0 Serialize the 'order' object for your legacy bookkeeping system");
-console.log("-------------------------------------------------------------------\n");
-
-serialized = serializer().serialize(order, descriptors);
+serialized = serializer().serialize(invo, descriptors);
 
 console.log(serialized + '\n');
 
-console.log("\n6.0 Deserialize the 'order' object from the generated output string");
-console.log("-------------------------------------------------------------------\n");
+
+
+console.log("\n6.0 Re-generate the 'invoice' object from a given output string");
+console.log("---------------------------------------------------------------\n");
 
 console.log(serializer().deserialize(serialized, descriptors));
-*/
+
